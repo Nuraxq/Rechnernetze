8e526351e11c10c6c81fb283aca3ecffaccf2d08
Nuraxq <niklas.bueschelmann@web.de>
2025-11-07 13:48:41 +0100

    A&b Fertig

--- Skillcheck/task-a/client.py
+++ Skillcheck/task-a/client.py
@@ -1,6 +1,54 @@
+import socket 
+import struct
+import time
+
 def main():
-    # Place your implementation here
+
+    sec_num = 0 #Seq nummer die wir mitgeben
+    number_rtt = 0 # RTT's mitzählen
+    rttimes = [] 
+    IP = 'localhost'
+    PORT = 4711
+
+    sock = socket.socket(socket.AF_INET,socket.sock_DGRAM) # Socket aufsetzen
+    sock.settimeout(3)
+
+    while number_rtt < 11:
+        data = struct.pack("!4si",b'PING',sec_num)
+        try: 
+
+            first = time.time() # Zeit messen
+
+            sock.sendto(data,(IP,PORT)) # Daten verschicken
+
+            answer, nadress  = sock.recvfrom(1024) # PONG erwarten
+
+            second = time.time() # Zeit stoppen
+
+            timediff = (second-first) * 1000 # Zeit in MS
+
+            ansString, ansSeq = struct.unpack("4si",answer[0:8])
+
+            if str(ansString,'utf-8') == "PONG":
+                print(f"Seq {sec_num}: RTT ist {timediff}")
+
+                rttimes.append(timediff)
+                number_rtt = number_rtt +1
+
+            else:
+                print("Message doesnt begin with PONG")
+
+        except socket.timeout:
+            print(f"Message {sec_num} was lost")
+                
+        sec_num = sec_num + 1
+    
+    rttimes.sort()
+    print("Median der RTT's: {rttimes[len(rttimes)//2]}")
+
+    sock.close()
     pass
+
 
 
 if __name__ == "__main__":
--- Skillcheck/task-b/client.py
+++ Skillcheck/task-b/client.py
@@ -1,5 +1,30 @@
+import socket
+import struct
+
 def main():
-    # Place your implementation here
+    
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    IP = 'localhost'
+    PORT = 4711
+
+    sock.connect((IP,PORT))
+
+    last_time = 0    
+    byteString = b''
+
+    with open("temps.data",'rt') as file:
+        for line in file:
+            time, temp = line.split(' ')
+            time = (time)
+            temp = float(temp)
+        
+            if time - last_time >= 5000:
+                byteString += struct.pack("!qf",time,temp)
+                last_time = time
+             
+        sock.sendall(byteString)
+    
+    sock.close()
     pass
 
 
