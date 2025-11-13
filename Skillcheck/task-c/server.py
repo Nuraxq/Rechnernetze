f336dd2b41591c19e4a05b724f25774e0a048f99
Nuraxq <niklas.bueschelmann@web.de>
2025-11-13 22:42:22 +0100

    Update server.py

--- Skillcheck/task-c/server.py
+++ Skillcheck/task-c/server.py
@@ -1,46 +1,29 @@
-import socket,struct 
-
+import struct, socket
 def main():
-
-    t = 0
-
-    IP = 'localhost'
-    PORT = 4711
-
+    IP,PORT = 'localhost',4711
     sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
     sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-
     sock.bind((IP,PORT))
     sock.listen(5)
-
+    t = 0.0
     while True:
-        nsock, nadress = sock.accept()
-
+        nsock , nadress = sock.accept()
         while True:
-            #Daten Empfangen
             data = nsock.recv(1024)
-            #Falls Connection getrennt wurde
-            if not data:
+            if not data: 
                 break
-            #Fallunterscheidung der Befehle
             if len(data) == 9:
-                command,num = struct.unpack("!cd",data)
-                num = float(num)
-                if command == b'A':
+                com,num = struct.unpack("!cd",data)
+                if com == b'A':
                     t += num
-                elif command == b'M':
+                elif com == b'M':
                     t *= num
-        
             elif len(data) == 1:
-                command = struct.unpack("!c",data)[0]
-                if command == b'R':
+                com = struct.unpack("!c",data)[0] # unpack gibt tupel zurück
+                if com == b'R':
                     t = 0
-                elif command == b'G':
-                    package = struct.pack("!d",t)
-                    nsock.sendall(package)
+                elif com == b'G':
+                    nsock.sendall(struct.pack("!d",t))
     pass
-
-
 if __name__ == "__main__":
     main()
