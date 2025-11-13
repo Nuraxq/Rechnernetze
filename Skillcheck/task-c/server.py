75149b62b6984e4cb5bdb143742b36f656621f95
Nuraxq <niklas.bueschelmann@web.de>
2025-11-13 23:48:25 +0100

    Update server.py

--- Skillcheck/task-c/server.py
+++ Skillcheck/task-c/server.py
@@ -11,6 +11,7 @@ def main():
         while True:
             data = nsock.recv(1024)
             if not data: 
+                nsock.close()
                 break
             if len(data) == 9:
                 com,num = struct.unpack("!cd",data)
