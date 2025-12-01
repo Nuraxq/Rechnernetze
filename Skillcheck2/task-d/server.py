8a19735ef0183ab04d345c8107760b97d370ca83
Nuraxq <niklas.bueschelmann@web.de>
2025-12-01 18:52:22 +0100

    Update server.py

--- Skillcheck2/task-d/server.py
+++ Skillcheck2/task-d/server.py
@@ -1,34 +1,73 @@
 import socket, struct
 
-
 def main():
 
     server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     server.bind(('localhost',4711))
-
-    # Goofy doppel schleife überarbeiten!
-    while True:
-        data,adress = server.recvfrom(1024)
-        ch, len = struct.unpack("!cH",data[:3])
-        if (ch == b'D'):
-            curr_client = adress
-            session_active = True
-        while session_active:
-            with open("recieved.txt","wt") as file:
-
-
-
+    print("Server Started")
     
+    while True:
+        print("New Session started!")
 
+        with open("recieved.txt","w") as file:
+            current_client = None
+            while True:
 
-def byteParity(data: bytes):
+                data, adress = server.recvfrom(1024)
+                
+                #Set client
+                if current_client is None:
+                    current_client = adress
 
+                #Skip wrong client
+                if current_client != adress:
+                    continue
 
+                #Data Packet
+                if len(data) == 1:
+                    if data == b'F':
+                        print("Recieved finalize packet, resetting state")
+                        break
+                else:
+                    char, length = struct.unpack("!ch",data[:3])
+
+                    #Check for D, Split data
+                    if char == b'D':
+                        payload = data[3:3+length]
+                        lrc_byte = data[3+length]
+                        
+                        #We only use lower 7 bits
+                        text = ""
+                        for zeichen in data:
+                            text += chr(zeichen & 0b0111_1111)
+
+                        if isValid(payload,lrc_byte):
+                            print(f"Message: {text} | Error: No")
+                            file.write(text)
+                            server.sendto(b"A",(current_client))
+                        else:
+                            print(f"Message: {text} | Error: Yes") 
+                            server.sendto(b"E",(current_client))
+
+        
+def isValid(payload, lrc_byte):
+    for byte in payload:
+        if not byteParity(byte):
+            return False
+    if lrc(payload) != lrc_byte:
+        print("hier")
+        return False
+    return True
+
+def byteParity(data: int):
+    data_bits = data & 0b0111_1111
+    parity_bit = (data >> 7) & 1
+    return ((data_bits.bit_count() + parity_bit) % 2 ) == 0
 
 def lrc(data: bytes):
     lrc = 0
     for byte in data:
-        lrc ^= byte
+        lrc ^= (byte & 0b0111_1111)
     return lrc
 
 if __name__ == "__main__":
