257e60f4794b766e4db5d4d968966af39c3224b0
Nuraxq <niklas.bueschelmann@web.de>
2025-12-05 13:21:38 +0100

    Update server.py

--- Skillcheck2/task-c/server.py
+++ Skillcheck2/task-c/server.py
@@ -9,51 +9,46 @@ def main():
     args_parser.add_argument("--port", default=4711, type=int, help="Port to listen for incoming connections")
     args = args_parser.parse_args()
 
-    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    server.bind(("127.0.0.1",args.port))
-    server.listen(5)
-    socketlist = []
-    socketlist.append(server)
-
     totals = {}
-
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    server.bind(('localhost',args.port))
+    server.listen(5)
+    slist = []
+    slist.append(server)
+    totals = {}
     while True:
-        readable,writeable,error = select.select(socketlist,[],[],0)
-
+        readable,writeable,error = select.select(slist,[],[],0) # <- 0 am Ende!
         for sock in readable:
             if sock == server:
-                sockfd, addr = server.accept()
-                socketlist.append(sockfd)
-
+                nsock,address = server.accept()
+                slist.append(nsock)
             else:
-                data = sock.recv(1024)  
-                adress = sock.getpeername()
-                if adress not in totals:
-                    totals[adress] = 0
+                data = sock.recv(1024)
                 if not data:
-                    if sock in socketlist:
-                        socketlist.remove(sock)
-                        continue
-
-
-                if len(data) == 9:
+                    if sock in slist:
+                        slist.remove(sock)
+                    continue
+        
+                address = sock.getpeername()
+                if address not in totals:
+                    totals[address] = 0
+                    
+                if len(data) == 1:
+                    if data == b'R':
+                        totals[address] = 0
+                    elif data == b'G':
+                        sock.sendall(struct.pack("!d",totals[address]))
+                        
+                elif len(data) == 9:
                     char, num = struct.unpack("!cd",data)
                     if char == b'A':
-                        totals[adress] = totals[adress] + num
+                       totals[address] += num
                     elif char == b'M':
-                        totals[adress] = totals[adress] * num
-                elif len(data) == 1:
-                    char = struct.unpack("!c",data)[0]
-                    if char == b'G':
-                        sock.sendall(struct.pack("!d",totals[adress]))
-                    elif char == b'R':
-                        totals[adress] = 0
-    for sock in socketlist:
+                        totals[address] *= num 
+    for sock in slist:
         sock.close()
-   
 
-
-            
 if __name__ == "__main__":
     main()
 
